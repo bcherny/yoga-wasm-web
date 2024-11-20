07d5fb1b5c9cdab934fa781bbaaf16390fa678eb
Boris Cherny <boris@anthropic.com>
2024-11-20 13:55:23 -0800

    commit buld fs

--- node.js
+++ node.js
@@ -1,9 +1,9 @@
-import { readFile } from "node:fs/promises";
-import { createRequire } from "node:module";
+import yoga from "./yoga.wasm" with { type: "file" };
+import { file } from "bun";
 import initYoga from "./index.js";
 
 const Yoga = await initYoga(
-  await readFile(createRequire(import.meta.url).resolve("./yoga.wasm"))
+  await file(yoga).arrayBuffer()
 );
 
 export * from "./yoga/javascript/src_js/generated/YGEnums.js";
